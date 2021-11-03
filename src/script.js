99f2763560dba699925002ec7ccfb51c2d30e61b
PontusVallgren <pontus.wallgren92@gmail.com>
2021-11-03 23:50:47 +0100

    added opacity and delay animation for mobile menu popup

--- src/script.js
+++ src/script.js
@@ -2,17 +2,20 @@
 const burgerOpen = document.querySelector(".burger-open-btn");
 const burgerClose = document.querySelector(".burger-close-btn");
 const navbar = document.querySelector(".navbar");
+const body = document.querySelector("body");
 
 // Events
 
 burgerOpen.addEventListener("click", () => {
   if (navbar.classList.contains("hidden")) {
-    navbar.classList.remove("hidden");
+    setTimeout(() => navbar.classList.remove("hidden"), 300);
+    body.classList.toggle("active");
   }
 });
 
 burgerClose.addEventListener("click", () => {
   if (!navbar.classList.contains("hidden")) {
     navbar.classList.add("hidden");
+    body.classList.toggle("active");
   }
 });
