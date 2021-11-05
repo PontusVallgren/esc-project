a69b08e29b8778f319ef146348ff9603cda9a632
PontusVallgren <pontus.wallgren92@gmail.com>
2021-11-05 17:25:01 +0100

    ReDone navbar popup function after design clarfication

--- src/script.js
+++ src/script.js
@@ -4,18 +4,33 @@ const burgerClose = document.querySelector(".burger-close-btn");
 const navbar = document.querySelector(".navbar");
 const body = document.querySelector("body");
 
+// Functions
+
+const createElements = () => {
+  const navbarBg = document.createElement("div");
+  navbarBg.setAttribute("class", "navbarBg");
+  body.append(navbarBg);
+  navbarBg.append(navbar);
+
+  return navbarBg;
+};
+
+const handleOpenMobileMenu = () => {
+  const navbarBg = createElements();
+  navbarBg.animate([{ opacity: 0 }, { opacity: 0.5 }], {
+    duration: 300,
+  });
+  setTimeout(() => navbar.classList.remove("hidden"), 300);
+};
+
+const handleCloseMobileMenu = () => {
+  const nvbg = document.querySelector(".navbarBg");
+  nvbg.removeChild(navbar);
+  body.removeChild(nvbg);
+  navbar.classList.add("hidden");
+};
+
 // Events
 
-burgerOpen.addEventListener("click", () => {
-  if (navbar.classList.contains("hidden")) {
-    setTimeout(() => navbar.classList.remove("hidden"), 300);
-    body.classList.toggle("active");
-  }
-});
-
-burgerClose.addEventListener("click", () => {
-  if (!navbar.classList.contains("hidden")) {
-    navbar.classList.add("hidden");
-    body.classList.toggle("active");
-  }
-});
+burgerOpen.addEventListener("click", handleOpenMobileMenu);
+burgerClose.addEventListener("click", handleCloseMobileMenu);
